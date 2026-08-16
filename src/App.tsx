9f2652a1e28321758ba190c9479c33a802ce3246
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-16 14:14:51 +0200

    removed coments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-// 1. Importera det som behövs från React Query
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { routeTree } from './routeTree.gen'
 
-// 2. Skapa din QueryClient (detta är "hjärnan" som håller all cachad data)
 const queryClient = new QueryClient()
 
 const router = createRouter({ routeTree })
@@ -17,7 +15,6 @@ declare module '@tanstack/react-router' {
 
 function App() {
   return (
-    // 3. Slå in hela din Router i QueryClientProvider och skicka med din client
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
     </QueryClientProvider>
