a032537efcc04533eccc974f47206195a91cec0d
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-18 13:59:45 +0200

    feat: integrate Auth0 for authentication and add router devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
 function App() {
+  const { VITE_AUTH0_DOMAIN, VITE_AUTH0_CLIENT_ID, VITE_AUTH0_AUDIENCE } =
+    import.meta.env;
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  )
+    <Auth0Provider
+      domain={VITE_AUTH0_DOMAIN}
+      clientId={VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: VITE_AUTH0_AUDIENCE,
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Auth0Provider>
+  );
 }
 
-export default App
+export default App;
