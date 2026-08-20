7c9335bc6b85e262563184f900f21f357fc74a1c
erikcerne <235554237+erikcerne@users.noreply.github.com>
2026-08-20 14:37:57 +0200

    feat: add UsernameModal component to root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { UsernameModal } from "../components/UsernameModal";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <main className="w-full">
         <Outlet />
+        <UsernameModal />
       </main>
       <TanStackRouterDevtools />
     </>
